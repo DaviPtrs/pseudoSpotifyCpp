d0b37036e2720f032f646e41ba822f5ebf3423c2
Davi Labgrad <davispetris@gmail.com>
2019-10-08 14:23:16 -0300

    arrumando essa disney do jairo

--- src/PlataformaDigital.cpp
+++ src/PlataformaDigital.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "../lib/utils.hpp"
 #include "PlataformaDigital.hpp"
 
@@ -86,4 +87,31 @@ void PlataformaDigital::exportarBiblioteca(){
 
 void PlataformaDigital::gerarRelatorios(){
 
+}
+
+void PlataformaDigital::carregaArquivoGenero(std::ifstream &infile)
+{
+    std::string sigla;
+    std::string nome;
+    while(!(infile.eof()))
+    {
+        getline(infile, sigla, ';');
+        getline(infile, nome);
+        this->listaGeneros.push_back(new Midia::Genero(sigla, nome));
+        
+    }
+
+    this->listaGeneros.pop_back();  // gambiarra bonita, gambiarra formosa
+
+}
+
+
+void PlataformaDigital::imprimeListaGenero()
+{
+    for(int i = 0; i < this->listaGeneros.size(); i++)
+    {
+        std::cout<<this->listaGeneros[i]->getSigla()<<';';
+        std::cout<<this->listaGeneros[i]->getNome()<<std::endl;
+       // std::cout<<"to com fome"<< i <<std::endl;
+    }
 }
